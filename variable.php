14aa6f1c69a0126877b604dbc2c7ea26734720fc
omendra rajput <omendra.rajput@hbwsl.com>
2023-07-11 19:22:02 +0530

    Added a div to place price for variable products

--- variable.php
+++ variable.php
@@ -29,6 +29,8 @@ do_action( 'woocommerce_before_add_to_cart_form' ); ?>
 	<?php if ( empty( $available_variations ) && false !== $available_variations ) : ?>
 		<p class="stock out-of-stock"><?php esc_html_e( 'This product is currently out of stock and unavailable.', 'woocommerce' ); ?></p>
 	<?php else : ?>
+		<div class="variation_price">
+		</div>
 		<table class="variations" cellspacing="0">
 			<tbody>
 				<?php foreach ( $attributes as $attribute_name => $options ) : ?>
